3bf6c2fba9a22e25c710bb453d2ecf11291e32c6
David Peterman <dpeterman@na.hhcpr.hilton.com>
2018-03-23 01:57:01 -0400

    started groundup tutorial

--- notesapp/src/index.js
+++ notesapp/src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import "./index.css";
+
+//function(gets optional data) {return a peice of UI} << React in a Nutshell, also component
+
+function Root(props) {
+    const postBody = 'this would be coming from the DB';
+    return (
+        <div>
+        <h1> HELLO {props.name} and {props.cohort}</h1>
+        <p> indent better</p>
+        <hr/>
+        <ListItem title="post title" body={postBody}> </ListItem>
+        <hr/> 
+        </div>
+    );
+}
+
+function ListItem(props) {
+    return (
+        <div> 
+            <h1>{props.title}</h1>
+            <div>{props.body}</div>
+        </div>
+
+    );
+}
+
+ReactDOM.render(<Root name = "BOZO" cohort="cs6" />, document.getElementById('app'))
