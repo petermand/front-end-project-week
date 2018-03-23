680379ac04734a02264af992435e89405830bd22
David Peterman <dpeterman@na.hhcpr.hilton.com>
2018-03-23 02:28:36 -0400

    finished list making

--- notesapp/src/index.js
+++ notesapp/src/index.js
@@ -6,21 +6,44 @@ import "./index.css";
 //function(gets optional data) {return a peice of UI} << React in a Nutshell, also component
 
 function Root(props) {
-    const postBody = 'this would be coming from the DB';
+    const posts = [
+        {
+            id: 1,
+            title: 'Post 1',
+            body: 'Body of post 1'
+        },
+        {
+            id: 2,
+            title: 'Post 2',
+            body: 'Body of post 2'
+        },
+        {
+            id: 3,
+            title: 'Post 3',
+            body: 'Body of post 3'
+        },
+        {
+            id: 4,
+            title: 'Post 4',
+            body: 'Body of post 4'
+        },
+    ]
     return (
         <div>
-        <h1> HELLO {props.name} and {props.cohort}</h1>
-        <p> indent better</p>
-        <hr/>
-        <ListItem title="post title" body={postBody}> </ListItem>
-        <hr/> 
+        <h1> 
+          HELLO {props.name} and {props.cohort}
+        </h1>
+        <p>indent better</p>
+        {posts.map((post) => {
+    return <ListItem key={post.id} title={post.title} body={post.body} />;
+        })}
         </div>
     );
 }
 
 function ListItem(props) {
     return (
-        <div> 
+        <div className="list-item"> 
             <h1>{props.title}</h1>
             <div>{props.body}</div>
         </div>
